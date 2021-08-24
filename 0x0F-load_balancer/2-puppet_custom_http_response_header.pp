23c2398b0a70668e806855506a6ac77db0ed952f
vanemcb <vanem2201@gmail.com>
2021-08-24 12:03:45 -0500

    syntax fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -16,14 +16,14 @@ file_line {'redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
-  line    => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
 file_line {'header':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
-  line    => '\tadd_header X-Served-By $hostname;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 file {'/var/www/html/index.html':
