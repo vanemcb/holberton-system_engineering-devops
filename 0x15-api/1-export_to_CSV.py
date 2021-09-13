6f167a39531ab4ef06d6080fffd2a2a7435de393
vanemcb <vanem2201@gmail.com>
2021-09-13 15:41:37 -0500

    Script created

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Script that exports data in the CSV format """
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
+
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            argv[1])).json()
+
+    count = 0
+    data = []
+    # for items in tasks:
+    #     data.append([str(name.get('name')), name.get('id'), items.get(
+    #         'completed'), items.get('title')])
+
+    h = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    for items in tasks:
+        dict_data = {h[0]: name.get('id'), h[1]: name.get(
+            'username'), h[2]: items.get(
+                'completed'), h[3]: items.get('title')}
+        data.append(dict_data)
+
+    with open('{}.csv'.format(name.get('id')), 'w', encoding='UTF8') as f:
+        writer = csv.DictWriter(f, fieldnames=h)
+
+        # write multiple rows
+        writer.writerows(data)
