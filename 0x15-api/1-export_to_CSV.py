762d2a4c49f6aa79aa9a81052911c6f1621c73a1
vanemcb <vanem2201@gmail.com>
2021-09-13 16:26:04 -0500

    Comments deleted

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,13 +20,6 @@ if __name__ == "__main__":
         data.append([(name.get("id")), name.get('username'), items.get(
             'completed'), items.get('title')])
 
-    # h = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    # for items in tasks:
-    #     dict_data = {h[0]: str(name.get('id')), h[1]: name.get(
-    #         'username'), h[2]: items.get(
-    #             'completed'), h[3]: items.get('title')}
-    #     data.append(dict_data)
-
     with open('{}.csv'.format(name.get('id')), 'w', encoding='UTF8') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
