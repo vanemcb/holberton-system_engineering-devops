83ca7b34d553b03cec16af72d163d5ac1045583a
vanemcb <vanem2201@gmail.com>
2021-09-13 15:50:03 -0500

    Space added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,16 +11,17 @@ if __name__ == "__main__":
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
 
     tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])).json()
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            argv[1])).json()
 
     count = 0
     list_tasks = []
     for items in tasks:
-        if items.get('completed') == True:
+        if items.get('completed') is True:
             list_tasks.append(items.get('title'))
             count += 1
 
-    print('Employee {} is done with tasks({}/{}):'.format(name.get('name'),
+    print('Employee {} is done with tasks({}/{}): '.format(name.get('name'),
           count, len(tasks)))
 
     for i in list_tasks:
