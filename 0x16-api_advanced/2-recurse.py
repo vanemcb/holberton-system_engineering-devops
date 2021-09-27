703386461792384addd656a7e89462987492aa0c
vanemcb <vanem2201@gmail.com>
2021-09-27 18:50:53 -0500

    Script created

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit  """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+            Safari/537.36"}
+    data = requests.get(
+        'http://www.reddit.com/r/{}/hot.json?limit=30'.format(
+            subreddit), headers=headers)
+
+    if data.status_code != 200:
+        print(None)
+    else:
+        after = data.json().get('data').get('after')
+        if after is not None:
+            for index in range(0, 30):
+                hot_list.append(data.json().get('data').get(
+                    'children')[index].get('data').get('title'))
+            recurse(subreddit, hot_list, after)
+        return(hot_list)
