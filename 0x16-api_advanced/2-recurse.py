1de32285870b61191fcb9f3b6652e6ae47603dc1
vanemcb <vanem2201@gmail.com>
2021-09-27 18:58:46 -0500

    Script created

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,14 +4,14 @@ containing the titles of all hot articles for a given subreddit  """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=""):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
             Safari/537.36"}
     data = requests.get(
-        'http://www.reddit.com/r/{}/hot.json?limit=30'.format(
-            subreddit), headers=headers)
+        'http://www.reddit.com/r/{}/hot.json?limit=30&after={}'.format(
+            subreddit, after), headers=headers)
 
     if data.status_code != 200:
         print(None)
