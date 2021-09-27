f1520f7cf7200c0d1a4286291f7ce47d84d95c36
vanemcb <vanem2201@gmail.com>
2021-09-27 17:23:20 -0500

    Script created

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" unction that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit """
+
+import requests
+
+
+def top_ten(subreddit):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+            Safari/537.36"}
+    data = requests.get(
+        'http://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=headers).json()
+
+    for index in range(0, 10):
+        print(data.get('data').get('children')[index].get('data').get('title'))
