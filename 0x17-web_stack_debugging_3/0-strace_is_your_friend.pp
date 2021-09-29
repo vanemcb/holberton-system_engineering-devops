9d068d457fa6609c8f294fd24cba296bf66e2e82
vanemcb <vanem2201@gmail.com>
2021-09-29 16:50:40 -0500

    Syntax fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # manifest to fix server syscall
 exec {'Fix file .php':
-  command  => 'sudo sed -i "s/phpp/php/" var/www/html/wp-settings.php;',
+  command  => 'sudo sed -i "s/phpp/php/" var/www/html/wp-settings.php;
+                      sudo service apache2 restart ',
   provider => shell,
 }
